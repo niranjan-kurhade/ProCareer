e51eaf81659e00fdb563e752a6c4c03cc43a228b
Shreyas0410 <shreyas.deshpande03@gmail.com>
2024-01-27 16:59:55 +0530

    added login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>  
+  <Auth0Provider
+    domain="dev-3xpdhmkgl8j200te.us.auth0.com"
+    clientId="p2NpJ6yLraxdFAeHN8kzd82JTkHdmaeo"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </Auth0Provider>
+  
 )
